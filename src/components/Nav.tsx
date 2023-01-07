d72b0198ef14fb89412a12ccd0172af316a36686
Jason Michael <67502978+jasonmic2000@users.noreply.github.com>
2023-01-07 16:47:09 +0530

    Remove tooltips. add theme and kbar toggles in nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,6 +6,7 @@ import {
   FiSun,
   FiMoon,
   FiZap,
+  FiCommand,
 } from "react-icons/fi";
 import { useRouter } from "next/router";
 import React from "react";
@@ -39,16 +40,12 @@ const NavbarItems = [
 const Nav = ({ path }: { path: string }) => {
   const router = useRouter();
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
   const { query } = useKBar();
-  const [tooltipVisibility, setTooltipVisibility] = useState([
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-  ]);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   return (
     <div className="flex h-full min-h-full w-full flex-col items-center justify-start pt-6">
@@ -60,56 +57,47 @@ const Nav = ({ path }: { path: string }) => {
                 <button
                   key={index}
                   className="relative flex w-full items-center justify-center rounded bg-zinc-800 shadow duration-300 ease-in-out hover:scale-110 hover:bg-zinc-800 hover:shadow-xl focus:bg-zinc-800 dark:bg-zinc-700 dark:hover:bg-zinc-700 dark:focus:bg-zinc-700"
-                  onMouseLeave={() => {
-                    const temp = [...tooltipVisibility];
-                    temp[index] = false;
-                    setTooltipVisibility(temp);
-                  }}
-                  onMouseEnter={() => {
-                    const temp = [...tooltipVisibility];
-                    temp[index] = true;
-                    setTooltipVisibility(temp);
-                  }}
                   onClick={() => router.push(item.slug)}
                 >
                   <div className="p-2">
                     <item.icon size="1rem" className="text-zinc-100" />
                   </div>
-                  {tooltipVisibility[index] && (
-                    <span className="absolute left-10 min-w-full rounded bg-zinc-800 p-[0.62rem] text-[0.75rem] leading-none text-zinc-200 shadow-xl dark:bg-zinc-700">
-                      {item.name}
-                    </span>
-                  )}
                 </button>
               ) : (
                 <button
                   key={index}
                   className="relative flex w-full items-center justify-center rounded bg-zinc-700 shadow duration-300 ease-in-out hover:scale-110 hover:bg-zinc-800 hover:shadow-xl focus:bg-zinc-800 dark:bg-zinc-800 dark:hover:bg-zinc-700 dark:focus:bg-zinc-700"
-                  onMouseLeave={() => {
-                    const temp = [...tooltipVisibility];
-                    temp[index] = false;
-                    setTooltipVisibility(temp);
-                  }}
-                  onMouseEnter={() => {
-                    const temp = [...tooltipVisibility];
-                    temp[index] = true;
-                    setTooltipVisibility(temp);
-                  }}
                   onClick={() => router.push(item.slug)}
                 >
                   <div className="p-2">
                     <item.icon size="1rem" className="text-zinc-100" />
                   </div>
-                  {tooltipVisibility[index] && (
-                    <span className="absolute top-10 rounded bg-zinc-800 p-[0.62rem] text-[0.75rem] leading-none text-zinc-200 shadow-xl dark:bg-zinc-700">
-                      {item.name}
-                    </span>
-                  )}
                 </button>
               )}
             </div>
           );
         })}
+        <div className="flex flex-row gap-12">
+          {mounted === true && (
+            <button
+              className="flex w-full items-center justify-center rounded bg-zinc-700 shadow duration-300 ease-in-out hover:scale-110 hover:bg-zinc-800 hover:shadow-xl dark:bg-zinc-800 dark:hover:bg-zinc-700"
+              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            >
+              <div className="p-2 text-zinc-100">
+                {theme === "dark" ? <FiSun /> : <FiMoon />}
+              </div>
+            </button>
+          )}
+          <button
+            className="flex w-full items-center justify-center rounded bg-zinc-700 shadow duration-300 ease-in-out hover:scale-110 hover:bg-zinc-800 hover:shadow-xl dark:bg-zinc-800 dark:hover:bg-zinc-700"
+            //   onClick={() => router.push(item.slug)}
+            // onClick={() => query.toggle}
+          >
+            <div className="p-2">
+              <FiCommand size="1rem" className="text-zinc-100" />
+            </div>
+          </button>
+        </div>
       </div>
     </div>
   );
