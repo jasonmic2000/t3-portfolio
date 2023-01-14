3c0659dd7f27a19749f7ef2f75eba4a96a521a3e
Jason Michael <67502978+jasonmic2000@users.noreply.github.com>
2023-01-14 14:52:42 +0530

    Custom scrollbar and typography prose implementation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,13 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("tailwind-scrollbar")({ nocompatible: true }),
+  ],
   darkMode: "class",
 };
