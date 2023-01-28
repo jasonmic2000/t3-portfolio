8f75498a1bf514727f19096b922f9f1ef1a62bc4
Jason Michael <67502978+jasonmic2000@users.noreply.github.com>
2023-01-28 17:32:11 +0530

    Format index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 import Layout from "../components/Layout";
-import { signIn, signOut, useSession } from "next-auth/react";
 import { getPinnedRepos } from "../lib/repos";
 import type { Repo } from "../lib/types";
 
-import { trpc } from "../utils/trpc";
-import Card from "../components/Card";
 import PinnedRepos from "../components/PinnedRepos";
+import Footer from "../components/Footer";
 
 const Home = (props: { pinnedRepos: Repo[] }) => {
-  const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
-
   return (
     <>
       <Layout>
@@ -41,38 +37,7 @@ const Home = (props: { pinnedRepos: Repo[] }) => {
               </div>
             </section>
             <PinnedRepos pinnedRepos={props.pinnedRepos} />
-          </div>
-          <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-            <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-              <span className="text-slate-900 dark:text-slate-50">Jason </span>
-              <span className="text-[hsl(280,100%,70%)]">Michael</span>
-            </h1>
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-              <Card href="https://create.t3.gg/en/usage/first-steps">
-                <>
-                  <h3 className="text-2xl font-bold">First Steps →</h3>
-                  <div className="text-lg">
-                    Just the basics - Everything you need to know to set up your
-                    database and authentication.
-                  </div>
-                </>
-              </Card>
-              <Card href="https://create.t3.gg/en/introduction">
-                <>
-                  <h3 className="text-2xl font-bold">Documentation →</h3>
-                  <div className="text-lg">
-                    Learn more about Create T3 App, the libraries it uses, and
-                    how to deploy it.
-                  </div>
-                </>
-              </Card>
-            </div>
-            <div className="flex flex-col items-center gap-2">
-              <p className="text-2xl text-white">
-                {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-              </p>
-              <AuthShowcase />
-            </div>
+            <Footer />
           </div>
         </main>
       </Layout>
@@ -91,27 +56,3 @@ export async function getStaticProps() {
 }
 
 export default Home;
-
-const AuthShowcase: React.FC = () => {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => signOut() : () => signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-};
