caee319c95537d45e7e1970be7b641268f4ea628
Jason Michael <67502978+jasonmic2000@users.noreply.github.com>
2023-01-29 21:48:26 +0530

    Template coming soon pages for about and dashboard

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,22 @@
+import Layout from "../components/Layout";
+import Footer from "../components/Footer";
+
+export default function Dashboard() {
+  return (
+    <Layout>
+      <div className="relative flex h-full min-h-screen w-full flex-col items-center p-8">
+        <section className="prose mt-16 mb-12 flex w-full flex-col justify-between gap-6 md:mt-0 lg:mt-0">
+          <div>
+            <h1 className="mb-3 leading-none text-slate-900 dark:text-slate-50">
+              Dashboard
+            </h1>
+            <p className="m-0 leading-tight text-zinc-700 dark:text-zinc-400">
+              🚧This page is currently under construction.🚧
+            </p>
+          </div>
+        </section>
+        <Footer />
+      </div>
+    </Layout>
+  );
+}
