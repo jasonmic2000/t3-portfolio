ca74d009ab6d4b6caad01b5da68cc8cfbd2c4b68
Jason Michael <67502978+jasonmic2000@users.noreply.github.com>
2023-02-11 12:58:55 +0530

    Link styling for About Page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,7 +9,7 @@ export default function About(props: { html: string }) {
       <div className="relative flex h-full min-h-screen w-full flex-col items-center p-8">
         <section className="prose mt-16 mb-12 flex w-full flex-col justify-between gap-6 md:mt-0 lg:mt-0">
           <div
-            className="prose-headings:leading-none prose-h1:mb-3 prose-h1:text-slate-900 prose-h2:mt-0 prose-p:m-0 prose-p:leading-tight prose-p:text-zinc-700 dark:prose-h1:text-slate-50 dark:prose-p:text-zinc-400"
+            className="prose-headings:leading-none prose-h1:mb-3 prose-h1:text-slate-900 prose-p:leading-tight prose-p:text-zinc-700 prose-a:text-zinc-600 prose-a:decoration-dotted prose-a:underline-offset-4 prose-a:duration-300 hover:prose-a:text-fuchsia-400 dark:prose-h1:text-slate-50 dark:prose-h4:text-zinc-100 dark:prose-p:text-zinc-300 dark:prose-a:text-zinc-200 dark:hover:prose-a:text-fuchsia-400"
             dangerouslySetInnerHTML={{ __html: props.html }}
           ></div>
         </section>
