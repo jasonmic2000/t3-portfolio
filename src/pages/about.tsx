ecb48c08abbdd16c46482cc83b0c4d3f14ef0704
Jason Michael <67502978+jasonmic2000@users.noreply.github.com>
2023-02-11 11:09:07 +0530

    Update about page
    Update about component to use md as props for content

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,22 +1,39 @@
 import Layout from "../components/Layout";
 import Footer from "../components/Footer";
+import fs from "fs";
+import showdown from "showdown";
 
-export default function About() {
+export default function About(props: { html: string }) {
   return (
     <Layout>
       <div className="relative flex h-full min-h-screen w-full flex-col items-center p-8">
         <section className="prose mt-16 mb-12 flex w-full flex-col justify-between gap-6 md:mt-0 lg:mt-0">
-          <div>
-            <h1 className="mb-3 leading-none text-slate-900 dark:text-slate-50">
+          <div
+            className="prose-headings:leading-none prose-h1:mb-3 prose-h1:text-slate-900 prose-h2:mt-0 prose-p:m-0 prose-p:leading-tight prose-p:text-zinc-700 dark:prose-h1:text-slate-50 dark:prose-p:text-zinc-400"
+            dangerouslySetInnerHTML={{ __html: props.html }}
+          >
+            {/* <h1 className="mb-3 leading-none text-slate-900 dark:text-slate-50">
               About Me
             </h1>
             <p className="m-0 leading-tight text-zinc-700 dark:text-zinc-400">
               🚧This page is currently under construction.🚧
-            </p>
+            </p> */}
           </div>
         </section>
         <Footer />
       </div>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const markdownText = fs.readFileSync("src/content/about.md", "utf-8");
+  const converter = new showdown.Converter({ metadata: true });
+  const html = converter.makeHtml(markdownText);
+
+  return {
+    props: {
+      html,
+    },
+  };
 }
