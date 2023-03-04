8e94ac8a424348e58f9490d0dbb3abe3d16866b5
Jason Michael <67502978+jasonmic2000@users.noreply.github.com>
2023-03-04 15:58:04 +0530

    Card color minor changes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,7 +13,7 @@ const Card = ({
   return (
     <>
       <Link
-        className="flex max-w-xs flex-col gap-4 rounded-xl bg-zinc-400/20 p-4 text-black shadow-lg shadow-zinc-800/10 duration-200 hover:scale-[103%] hover:bg-zinc-300 hover:shadow-2xl hover:shadow-zinc-800/10 dark:bg-zinc-500/20 dark:text-white dark:shadow-zinc-200/5 dark:hover:bg-white/20 dark:hover:shadow-zinc-200/5"
+        className="flex max-w-xs flex-col gap-4 rounded-xl bg-zinc-500/20 p-4 text-black shadow-lg shadow-zinc-800/10 duration-200 hover:scale-[103%] hover:bg-zinc-200 hover:shadow-2xl hover:shadow-zinc-800/10 dark:bg-zinc-500/20 dark:text-white dark:shadow-zinc-200/5 dark:hover:bg-white/20 dark:hover:shadow-zinc-200/5"
         href={href}
         target={target ? target : ""}
       >
